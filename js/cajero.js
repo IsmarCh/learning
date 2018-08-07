5ebfd4cb0ecec0f783f702f8bde31ff11cd6d382
Omar Crespo <crespomerchano@gmail.com>
2018-08-07 18:43:16 -0400

    prettier

--- js/cajero.js
+++ js/cajero.js
@@ -1,40 +1,42 @@
 // Obtención de Elementos
-let inputClave = document.getElementById('clave')
-let claveDiv = document.getElementById('claveDiv')
-let buttonClave = document.getElementById('aceptClave')
+const inputClave = document.getElementById('clave')
+const claveDiv = document.getElementById('claveDiv')
+const buttonClave = document.getElementById('aceptClave')
 
-let operacionDiv = document.getElementById('operacionDiv')
-let operation = {
+const buttonContinuar = document.getElementById('continue')
+const operacionDiv = document.getElementById('operacionDiv')
+const operation = {
     consulta: document.getElementById('selecConsulta'),
     retiro: document.getElementById('selecRetiro')
 }
-let buttonContinuar = document.getElementById('continue')
 
-let montoDiv = document. getElementById('montoDiv')
-let monto = document.getElementById('monto')
-let buttonMonto = document.getElementById('aceptMonto')
+const montoDiv = document. getElementById('montoDiv')
+const monto = document.getElementById('monto')
+const buttonMonto = document.getElementById('aceptMonto')
 
-let cuentaDiv = document.getElementById('cuentaDiv')
-let tipoCuenta = {
+const buttonNext = document.getElementById('next')
+const cuentaDiv = document.getElementById('cuentaDiv')
+const tipoCuenta = {
     ahorro: document.getElementById('ahorro'),
     corriente: document.getElementById('corriente')
 }
-let buttonNext = document.getElementById('next')
 
-let cedulaDiv = document.getElementById('cedulaDiv')
-let inputCedula = document.getElementById('cedula')
-let buttonCedula = document.getElementById('aceptCedula')
+const cedulaDiv = document.getElementById('cedulaDiv')
+const inputCedula = document.getElementById('cedula')
+const buttonCedula = document.getElementById('aceptCedula')
 
-let textFinal = document.getElementById('exitosaDiv')
+const textFinal = document.getElementById('exitosaDiv')
 
 // Escuchadores de Evento 
 buttonClave.addEventListener('click', function () {
-if (inputClave.value == '') {
+    if (inputClave.value == '') {
         alert("Ingrese su clave")
         return
-}
-        operacionDiv.classList = ["show"]
+    }
+
+    operacionDiv.classList = ["show"]
 }) 
+
 buttonContinuar.addEventListener('click', function () {
     if (!operation.consulta.checked & !operation.retiro.checked) {
         alert("Selecciona el tipo de operación que vayas a realizar")
@@ -42,32 +44,39 @@ buttonContinuar.addEventListener('click', function () {
     }
     
     if (operation.consulta.checked == false) {
-        montoDiv.classList = ["show"] 
+        montoDiv.classList = ["show"]
     }
+
     if (operation.retiro.checked == false) {
         montoDiv.classList = ["hide"]
         cuentaDiv.classList = ["show"]
     }
 })
+
 buttonMonto.addEventListener('click', function () {
     if (monto.value == '') {
         alert('Ingresa un monto')
         return
     }
+
     cuentaDiv.classList = ["show"]
 })
+
 buttonNext.addEventListener('click', function () {
     if (!tipoCuenta.ahorro.checked & !tipoCuenta.corriente.checked) {
         alert('Selecciona el tipo de cuenta')
         return
     }
+    
     cedulaDiv.classList = ["show"]
 })
+
 buttonCedula.addEventListener('click', function () {
     if (inputCedula.value == '') {
         alert('Ingresa los dos primeros dígitos de tu cédula')
         return
     }
+
     claveDiv.classList = ["hide"]
     operacionDiv.classList = ["hide"]
     montoDiv.classList = ["hide"]
